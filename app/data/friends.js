b8516199eed4e0d904d6a87dee5eaeb7eec76974
trcka <michael.trcka@ttu.edu>
2017-08-04 19:04:03 -0500

    deploying

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var users = [
+  {
+  name: "Michael Trcka",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAxzAAAAJGM2YjM0N2QwLTQyMDMtNGNiYi04ZDllLTNhNGJjNzM1N2ZhOQ.jpg",
+  score: ["5","5","0","4","3","5","5","1","0","4",],
+  },
+  {
+    name: "The Dude",
+    photo: "http://orig11.deviantart.net/a2ee/f/2015/196/6/3/the_dude_by_brucewhite-d91ekro.jpg",
+    score: ['5','5','5','5','5','5','5','5','5','5']
+  },
+  {
+    name: 'Sarah Trcka',
+    photo: "https://s-media-cache-ak0.pinimg.com/avatars/sarahtrcka18_1459825149_140.jpg",
+    score: ['5','5','5','3','4','1','2','0','5','10']
+  }
+];
+
+module.exports = users;
